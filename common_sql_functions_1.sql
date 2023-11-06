7a836170d94c36c56b7b5142ec76fb3bb37df79a
Fiona-Huang <78253153+Fiona-Huang@users.noreply.github.com>
2023-11-06 21:35:54 +0800

    Update common_sql_functions_1.sql

--- common_sql_functions_1.sql
+++ common_sql_functions_1.sql
@@ -23,3 +23,75 @@ SELECT * FROM (
   AND regexp_contains(app_version, "^23.1")
 )
 WHERE total_variant_per_user = 1
+
+
+-- aggregation from events level to session level
+WITH session_funnel AS (
+  SELECT
+  country,
+  region,
+  platform,
+  session_id,
+  menu_page_loaded > 0 AS stage_1, --returns boolean false/true
+  product_page_loaded > 0 AS stage_2,
+  cart_page_loaded > 0 AS stage_3,
+  checkout_page_loaded > 0 AS stage_4,
+  transaction_page_loaded > 0 AS final_stage,
+FROM session_level_table_1
+WHERE date_utc BETWEEN "2023-01-01" AND "2023-01-30"
+),
+
+interest_vendors AS (
+SELECT DISTINCT
+vendor_id,
+country,
+business_type
+FROM vendor_table_1
+LEFT JOIN vendor_category USING (vendor_id)
+)
+ 
+,events AS (
+  SELECT DISTINCT 
+  e.country,
+  e.session_id,
+  e.event_action,
+  e.screen_type,
+  e.vendorid,
+  MAX(IF(array.name = "array_name_1", array.value, NULL)) AS array_name_1,
+  MAX(IF(array.name = "array_name_2", array.value, NULL)) AS array_name_2
+FROM events_table_1 AS e
+LEFT JOIN UNNEST(e.array) AS array
+WHERE e.partition_date BETWEEN "2023-01-01" AND "2023-01-30"
+  AND e.event_action IN ("interested_event_1", "interested_event_2")
+  AND ev.name IN ("array_name_1", "array_name_2")
+GROUP BY 1,2,3,4,5
+ )
+
+ ,events_2 AS (
+  SELECT
+  events.*
+ FROM events LEFT JOIN interest_vendors
+ USING (country, vendor_id)
+WHERE interest_vendors.business_type IN ("group_1", "group_2")
+ )
+
+,session_level AS (
+SELECT DISTINCT
+country,
+session_id, 
+IFNULL(MAX(IF(event_action = "interested_event_1" AND array_name_1 = "string", 1, 0)),0) AS fulfilled_criteria_1,
+IFNULL(MAX(IF(event_action = "interested_event_2" AND array_name_2 = "abcd", 1, 0)),0) AS fulfilled_criteria_2,
+IFNULL(MAX(IF(event_action = "interested_event_2" AND array_name_2 = "efgh", 1, 0)),0) AS fulfilled_criteria_3,
+FROM events_2
+GROUP BY 1
+)
+
+SELECT 
+country,
+COUNT(DISTINCT(IF((fulfilled_criteria_1 > 0), session_id, NULL))) AS sessions_with_only_criteria_1,
+COUNT(DISTINCT(IF((fulfilled_criteria_1 > 0 AND fulfilled_criteria_2 > 0), session_id, NULL))) AS sessions_with_criteria_1_and_2,
+COUNT(DISTINCT(IF((fulfilled_criteria_2 = 0 AND fulfilled_criteria_3 > 0), session_id, NULL))) AS sessions_with_criteria_3_but_not_2,
+FROM session_funnel
+LEFT JOIN session_level USING (session_id)
+WHERE (stage_1 AND stage_2 AND stage_3 AND NOT stage_4 AND NOT final_stage)
+GROUP BY 1
