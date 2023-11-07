ffe142fc46d270c74730d82a0f5ff3b85c4319f0
Fiona-Huang <78253153+Fiona-Huang@users.noreply.github.com>
2023-11-07 17:23:58 +0800

    Update leetcode_solutions_basic_joins.sql

--- leetcode_solutions_basic_joins.sql
+++ leetcode_solutions_basic_joins.sql
@@ -1,4 +1,37 @@
 -- Basic Joins (Medium Difficulty) Confirmation Rate
+
+-- Input: 
+-- Signups table:
+-- +---------+---------------------+
+-- | user_id | time_stamp          |
+-- +---------+---------------------+
+-- | 3       | 2020-03-21 10:16:13 |
+-- | 7       | 2020-01-04 13:57:59 |
+-- | 2       | 2020-07-29 23:09:44 |
+-- | 6       | 2020-12-09 10:39:37 |
+-- +---------+---------------------+
+-- Confirmations table:
+-- +---------+---------------------+-----------+
+-- | user_id | time_stamp          | action    |
+-- +---------+---------------------+-----------+
+-- | 3       | 2021-01-06 03:30:46 | timeout   |
+-- | 3       | 2021-07-14 14:00:00 | timeout   |
+-- | 7       | 2021-06-12 11:57:29 | confirmed |
+-- | 7       | 2021-06-13 12:58:28 | confirmed |
+-- | 7       | 2021-06-14 13:59:27 | confirmed |
+-- | 2       | 2021-01-22 00:00:00 | confirmed |
+-- | 2       | 2021-02-28 23:59:59 | timeout   |
+-- +---------+---------------------+-----------+
+-- Output: 
+-- +---------+-------------------+
+-- | user_id | confirmation_rate |
+-- +---------+-------------------+
+-- | 6       | 0.00              |
+-- | 3       | 0.00              |
+-- | 7       | 1.00              |
+-- | 2       | 0.50              |
+-- +---------+-------------------+
+
 SELECT
 s.user_id,
 CASE WHEN c.user_id IS NULL
@@ -10,6 +43,26 @@ LEFT JOIN Confirmations c ON c.user_id = s.user_id
 GROUP BY s.user_id
 
 -- Basic Joins (Medium Difficulty) Managers with at least 5 Direct Reports
+    
+-- Input: 
+-- Employee table:
+-- +-----+-------+------------+-----------+
+-- | id  | name  | department | managerId |
+-- +-----+-------+------------+-----------+
+-- | 101 | John  | A          | None      |
+-- | 102 | Dan   | A          | 101       |
+-- | 103 | James | A          | 101       |
+-- | 104 | Amy   | A          | 101       |
+-- | 105 | Anne  | A          | 101       |
+-- | 106 | Ron   | B          | 101       |
+-- +-----+-------+------------+-----------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | John |
+-- +------+
+
 WITH manager AS (
     SELECT
     managerId,
