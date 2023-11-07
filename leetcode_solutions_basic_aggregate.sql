a02ae7323ff29e19c945eec8b5a8d5f927a11454
Fiona-Huang <78253153+Fiona-Huang@users.noreply.github.com>
2023-11-07 17:22:05 +0800

    Update leetcode_solutions_basic_aggregate.sql

--- leetcode_solutions_basic_aggregate.sql
+++ leetcode_solutions_basic_aggregate.sql
@@ -1,6 +1,25 @@
 -- Basic Aggregate Functions (Medium Difficulty) Monthly Transactions I
 --  find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
 
+-- Input: 
+-- Transactions table:
+-- +------+---------+----------+--------+------------+
+-- | id   | country | state    | amount | trans_date |
+-- +------+---------+----------+--------+------------+
+-- | 121  | US      | approved | 1000   | 2018-12-18 |
+-- | 122  | US      | declined | 2000   | 2018-12-19 |
+-- | 123  | US      | approved | 2000   | 2019-01-01 |
+-- | 124  | DE      | approved | 2000   | 2019-01-07 |
+-- +------+---------+----------+--------+------------+
+-- Output: 
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | month    | country | trans_count | approved_count | trans_total_amount | approved_total_amount |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+-- | 2018-12  | US      | 2           | 1              | 3000               | 1000                  |
+-- | 2019-01  | US      | 1           | 1              | 2000               | 2000                  |
+-- | 2019-01  | DE      | 1           | 1              | 2000               | 2000                  |
+-- +----------+---------+-------------+----------------+--------------------+-----------------------+
+
 SELECT
 DATE_FORMAT(trans_date, '%Y-%m') AS month,
 country,
@@ -18,6 +37,26 @@ DATEDIFF(day, ‘2022-01-01', ‘2022-01-02')
   
 -- Basic Aggregate Functions (Medium Difficulty) Immediate Food Delivery II
   -- find the percentage of immediate orders in the first orders of all customers
+  
+-- Input: 
+-- Delivery table:
+-- +-------------+-------------+------------+-----------------------------+
+-- | delivery_id | customer_id | order_date | customer_pref_delivery_date |
+-- +-------------+-------------+------------+-----------------------------+
+-- | 1           | 1           | 2019-08-01 | 2019-08-02                  |
+-- | 2           | 2           | 2019-08-02 | 2019-08-02                  |
+-- | 3           | 1           | 2019-08-11 | 2019-08-12                  |
+-- | 4           | 3           | 2019-08-24 | 2019-08-24                  |
+-- | 5           | 3           | 2019-08-21 | 2019-08-22                  |
+-- | 6           | 2           | 2019-08-11 | 2019-08-13                  |
+-- | 7           | 4           | 2019-08-09 | 2019-08-09                  |
+-- +-------------+-------------+------------+-----------------------------+
+-- Output: 
+-- +----------------------+
+-- | immediate_percentage |
+-- +----------------------+
+-- | 50.00                |
+-- +----------------------+
 
 WITH customer_order_level_table AS (
     SELECT
